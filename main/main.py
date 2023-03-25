73a985b79808420d6133e2553871109c7be6ac34
John Doe <beyondthestars628@gmail.com>
2023-03-25 01:05:34 -0400

    wg

---
+++ main/main.py
@@ -0,0 +1,21 @@
+import math
+
+# //literally just started learning python this is extremely disorienting
+
+alist = ["bruh", 23, "bruh", "what thehell", list, 3.4] # HOW
+print(alist)
+
+testvar = math.sqrt(4.)
+print(testvar)
+
+for x in alist:
+    if type(x) == str: # can be replaced by isinstance(x, str)
+        print("is string")
+    else:
+        print("not string :-)")
+
+    # str object acts as array of chars; ex:
+aString = "theres no way"
+print(aString[3])
+print(aString[1:9])
+
